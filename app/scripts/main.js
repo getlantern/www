dd2cc5551b320d2628fb20192f790d822a4d4147
fffw <fffw@getlantern.org>
2015-07-11 04:40:41 -0600

    improve os detection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,14 +17,16 @@ $(function() {
   var os = platform.os.architecture + platform.os.family;
   var os_links = [
     {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
-    {regexp: "OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
-    {regexp: "32Linux", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
-    {regexp: "64Linux", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"}
+    {regexp: "[2-6]{2}OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
+    {regexp: "32.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
+    {regexp: "64.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"},
+    {regexp: ".*", link: "https://github.com/getlantern/lantern/wiki/Lantern-Beta-Versions#download-links"}
   ];
   for (i = 0; i < os_links.length; ++i) {
     re = new RegExp(os_links[i].regexp);
     if (os.match(re)) {
       $("#download-button").attr("href", os_links[i].link);
+      break;
     }
   }
 });
