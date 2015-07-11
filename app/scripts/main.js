88f0dd78c246a214e5be65ee582b0bce08907577
fffw <fffw@getlantern.org>
2015-07-11 04:34:02 -0600

    detect os to download

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,4 +14,17 @@ $(function() {
   var labels = [
     "example-label"
   ];
+  var os = platform.os.architecture + platform.os.family;
+  var os_links = [
+    {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
+    {regexp: "OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
+    {regexp: "32Linux", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
+    {regexp: "64Linux", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"}
+  ];
+  for (i = 0; i < os_links.length; ++i) {
+    re = new RegExp(os_links[i].regexp);
+    if (os.match(re)) {
+      $("#download-button").attr("href", os_links[i].link);
+    }
+  }
 });
