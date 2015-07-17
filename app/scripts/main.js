8f1337b5c80a40a3c1a11d4cb3228368b67353ca
fffw <fffw@getlantern.org>
2015-07-18 00:51:21 +0800

    make send email button work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,39 @@
-var init_mandrill = function() {
-  mandrill_client = new mandrill.Mandrill('fmYlUdjEpGGonI4NDx9xeA');
-  var template_name = "download-link-from-lantern-website";
-  var template_content = [{
-    "name": "example name",
-    "content": "example content"
-  }];
-  var from_email = "admin@getlantern.org";
-  var from_name = "Lantern";
-  var subject = "Your Lantern download link";
-  var code = "<div>example code</div>";
-  var text = "Example text content";
-  var publish = false;
-  var labels = [
-    "example-label"
-  ];
-};
+(function() {
+  var init_mandrill = function() {
+    var mandrill_client = new mandrill.Mandrill('fmYlUdjEpGGonI4NDx9xeA');
+    var template_name = "download-link-from-lantern-website";
+    var template_content = [{
+      "name": "example name",
+      "content": "example content"
+    }];
+    $("#send-download-link-button").click(function(event) {
+      event.preventDefault();
+      var email = $("#send-download-link-email").val();
+      var message = { "to": [ { "email": email } ] };
+      mandrill_client.messages.sendTemplate({"template_name": template_name, "template_content": template_content, "message": message});
+    });
+  };
 
-var set_download_link = function() {
-  var os = platform.os.architecture + platform.os.family;
-  var os_links = [
-    {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
-    {regexp: "[2-6]{2}OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
-    {regexp: "32.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
-    {regexp: "64.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"},
-    {regexp: ".*", link: "https://github.com/getlantern/lantern/wiki/Lantern-Beta-Versions#download-links"}
-  ];
-  for (i = 0; i < os_links.length; ++i) {
-    re = new RegExp(os_links[i].regexp);
-    if (os.match(re)) {
-      $("#download-button").attr("href", os_links[i].link);
-      break;
+  var set_download_link = function() {
+    var os = platform.os.architecture + platform.os.family;
+    var os_links = [
+      {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
+      {regexp: "[2-6]{2}OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
+      {regexp: "32.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
+      {regexp: "64.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"},
+      {regexp: ".*", link: "https://github.com/getlantern/lantern/wiki/Lantern-Beta-Versions#download-links"}
+    ];
+    for (i = 0; i < os_links.length; ++i) {
+      re = new RegExp(os_links[i].regexp);
+      if (os.match(re)) {
+        $("#download-button").attr("href", os_links[i].link);
+        break;
+      }
     }
-  }
-};
+  };
 
-$(function() {
-  set_download_link();
-  init_mandrill();
-});
+  $(function() {
+    set_download_link();
+    init_mandrill();
+  });
+})();
