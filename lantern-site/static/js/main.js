68b58f15892e7cfb60ddb1ba7cda1cf4783adb6b
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-07-18 18:21:46 -0500

    Set download link.

--- lantern-site/static/js/main.js
+++ lantern-site/static/js/main.js
@@ -5,6 +5,24 @@ $(document).ready(function(){
     "zh-CN": "download-link-from-lantern-website-zh-cn"
   };
 
+  var set_download_link = function() {
+    var os = platform.os.architecture + platform.os.family;
+    var os_links = [
+      {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
+      {regexp: "[2-6]{2}OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
+      {regexp: "32.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
+      {regexp: "64.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"},
+      {regexp: ".*", link: "https://github.com/getlantern/lantern/wiki/Lantern-Beta-Versions#download-links"}
+    ];
+    for (i = 0; i < os_links.length; ++i) {
+      re = new RegExp(os_links[i].regexp);
+      if (os.match(re)) {
+        $("#download-button").attr("href", os_links[i].link);
+        break;
+      }
+    }
+  };
+
   var init_mandrill = function() {
     var mandrill_client = new mandrill.Mandrill('fmYlUdjEpGGonI4NDx9xeA');
     var lang = navigator.language || navigator.userLanguage;
@@ -59,8 +77,7 @@ $(document).ready(function(){
     return false;
   });
 
+  set_download_link();
   init_mandrill();
-
-
   language_chooser();
 });
