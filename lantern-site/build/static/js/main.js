b63e7ba9db589a334b46427fde9366f62cdc650b
José Carlos Nieto <jose.carlos@menteslibres.net>
2015-07-19 16:00:02 -0500

    Latest build.

--- lantern-site/build/static/js/main.js
+++ lantern-site/build/static/js/main.js
@@ -5,6 +5,28 @@ $(document).ready(function(){
     "zh-CN": "download-link-from-lantern-website-zh-cn"
   };
 
+  var set_download_link = function() {
+    var os = platform.os.architecture + platform.os.family;
+    var os_links = [
+      {regexp: "[2-6]{2}[Ww]indows", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.exe"},
+      {regexp: "[2-6]{2}OS X", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta.dmg"},
+      {regexp: "32.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-32.deb"},
+      {regexp: "64.*", link: "https://s3.amazonaws.com/lantern/lantern-installer-beta-64.deb"},
+      {regexp: ".*", link: "https://github.com/getlantern/lantern/wiki/Lantern-Beta-Versions#download-links"}
+    ];
+    for (i = 0; i < os_links.length; ++i) {
+      re = new RegExp(os_links[i].regexp);
+      if (os.match(re)) {
+        $("#download-button").attr("href", os_links[i].link);
+        break;
+      }
+    }
+
+    $('#download-button').on('click', function() {
+      ga('send', 'event', 'button', 'click', 'download');
+    });
+  };
+
   var init_mandrill = function() {
     var mandrill_client = new mandrill.Mandrill('fmYlUdjEpGGonI4NDx9xeA');
     var lang = navigator.language || navigator.userLanguage;
@@ -34,10 +56,24 @@ $(document).ready(function(){
   };
 
   var language_chooser = function() {
+    var lang = "en";
+
     $("#language-chooser").change(function() {
       var lang = $(this).find("option:selected").val() || "en";
-      $("[data-localize]").localize("locale/lang", { language: lang });
+      $("[data-localize]").localize("/static/locale/lang", { language: lang });
+      if (Modernizr.localstorage) {
+        window.localStorage.setItem("lang", lang);
+      }
     });
+
+    if (Modernizr.localstorage) {
+      lang = window.localStorage.getItem("lang");
+      if (lang) {
+        $("#language-chooser").val(lang);
+      }
+    };
+
+    $("[data-localize]").localize("/static/locale/lang", {language: lang});
   };
 
   $('.question a').click(function(){
@@ -45,9 +81,7 @@ $(document).ready(function(){
     return false;
   });
 
+  set_download_link();
   init_mandrill();
-
-  $("[data-localize]").localize("locale/lang");
-
   language_chooser();
 });
