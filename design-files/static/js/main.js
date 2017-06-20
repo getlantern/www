9bed0f6967d0d6201b4011e6a46a14739d1fed24
joesis <joesis@getlantern.org>
2017-06-20 16:24:23 +0800

    add raw design files

---
+++ design-files/static/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+  });
+  
+  $('.question a').click(function(){
+    $(this).closest('div').toggleClass('show');
+    return false;
+  });
+  
+
+			function DropDown(el) {
+				this.dd = el;
+				this.initEvents();
+			}
+			DropDown.prototype = {
+				initEvents : function() {
+					var obj = this;
+
+					obj.dd.on('click', function(event){
+						$(this).toggleClass('active');
+						event.stopPropagation();
+					});	
+				}
+			}
+
+			$(function() {
+
+				var dd = new DropDown( $('#dd') );
+
+				$(document).click(function() {
+					// all dropdowns
+					$('.select-dropdown').removeClass('active');
+				});
+
+			});
+
+  
+});
+	
