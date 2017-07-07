f18b9d865ea626e67985202ee74742be419812a5
joesis <joesis@getlantern.org>
2017-07-07 11:10:00 +0800

    add lang/ar.json and show more items in languages selector

--- bin/config.py
+++ bin/config.py
@@ -1,6 +1,9 @@
 _language_mapping = {
     'en_US': 'English (United States)',
-    'zh_CN': 'Chinese (Simplified)'
+    'zh_CN': 'Chinese (Simplified)',
+    'fr': 'French',
+    'es': 'Spanish',
+    'ar': 'Arabic'
 }
 _available_languages = _language_mapping.keys()
 template_vars = dict(
