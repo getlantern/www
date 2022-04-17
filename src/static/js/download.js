10b51cf166a48b8495590db916a78f93c8c5949c
Eli Yukelzon <reflog@gmail.com>
2022-04-17 14:08:49 +0300

    added separate 64/32 bit links

--- src/static/js/download.js
+++ src/static/js/download.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  var ua = new UAParser();
+  var cpu = ua.getCPU();
+  var os = ua.getOS();
 
   var set_download_link = function(cls, link) {
     $('.'+cls).attr('href', link);
@@ -6,26 +9,30 @@ $(document).ready(function(){
       ga('send', 'event', 'button', 'click', cls);
     });
   };
-  var os = platform.os.architecture + platform.os.family;
   var os_links = [
-    {regexp: "[2-6]{2}[Ww]indows", link: "../lantern-installer.exe"},
-    {regexp: ".*Android", link: "../lantern-installer.apk"},
-    {regexp: "[2-6]{2}OS X", link: "../lantern-installer.dmg"},
-    {regexp: "32.*", link: "../lantern-installer-32-bit.deb"},
-    {regexp: "64.*", link: "../lantern-installer-64-bit.deb"},
-    {regexp: ".*", link: "https://github.com/getlantern/lantern#lantern-"}
+    {os: "Windows", cpu: "ia32", link: "../lantern-installer-32-bit.exe"},
+    {os: "Windows", cpu: "amd64", link: "../lantern-installer-64-bit.exe"},
+    {os: "Android", link: "../lantern-installer.apk"},
+    {os: "Mac OS", link: "../lantern-installer.dmg"},    
+    {os: "Linux", cpu: "ia32", link: "../lantern-installer-32-bit.deb"},
+    { os: "Linux", cpu: "amd64", link: "../lantern-installer-64-bit.deb" },
+    {link: "https://github.com/getlantern/lantern#lantern-"}
   ];
-  set_download_link('download-windows', os_links[0].link);
-  set_download_link('download-android', os_links[1].link);
-  set_download_link('download-mac', os_links[2].link);
-  // 64 bit is mainstream now
-  set_download_link('download-ubuntu', os_links[4].link);
+  set_download_link('download-windows-32', os_links[0].link);
+  set_download_link('download-windows-64', os_links[1].link);
+  set_download_link('download-android', os_links[2].link);
+  set_download_link('download-mac', os_links[3].link);
+  set_download_link('download-ubuntu-64', os_links[5].link);
+  set_download_link('download-ubuntu-32', os_links[4].link);
 
   for (i = 0; i < os_links.length; ++i) {
-    re = new RegExp(os_links[i].regexp);
-    if (os.match(re)) {
-      set_download_link('download-btn', os_links[i].link);
-      set_download_link('download', os_links[i].link);
+    const l = os_links[i];
+    const os_ok = !l.os || (l.os === os.name);
+    const arch_ok = !l.cpu || (l.cpu === cpu.architecture);
+    console.log("comparing", l.os, os.name, l.cpu , cpu.architecture)
+    if (os_ok && arch_ok) {
+      set_download_link('download-btn', l.link);
+      set_download_link('download', l.link);
       break;
     }
   }
